fc62812717bae8b7ad40761831300350cfa61905
hanabe02 <hanaberu02@gmail.com>
2025-03-17 14:21:02 +0900

    Delete Nuxt_Back directory

--- Nuxt_Back/src/main/java/kr/co/seoulit/erp/logistic/sales/repository/SalesPlaneRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.seoulit.erp.logistic.sales.repository;
-
-import kr.co.seoulit.erp.logistic.sales.entity.SalesPlane;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface    SalesPlaneRepository extends JpaRepository<SalesPlane,String> {
-}
