cc7085ddaf1cf81b5499d3798dcb225f0f12dc93
hanabe02 <hanaberu02@gmail.com>
2025-07-15 02:56:23 +0900

    Delete HR_nuxt_backend directory

--- HR_nuxt_backend/src/main/java/kr/co/seoulit/erp/hr/base/applicationservice/JpaWorkplaceListService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.seoulit.erp.hr.base.applicationservice;
-
-import kr.co.seoulit.erp.hr.base.entity.WorkplaceListEntity;
-
-import java.util.ArrayList;
-
-public interface JpaWorkplaceListService {
-    public ArrayList<WorkplaceListEntity> findWorkplaceList(String companyCode);
-}
--- HR_nuxt_backend/src/main/java/kr/co/seoulit/erp/hr/salary/applicationservice/JpaSocialInsureService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.seoulit.erp.hr.salary.applicationservice;
-
-import kr.co.seoulit.erp.hr.salary.entity.SocialInsureEntity;
-
-import java.util.ArrayList;
-
-public interface JpaSocialInsureService {
-    ArrayList<SocialInsureEntity> findSocialInsureList(String year);
-}
